8ecb5d826a53f496e949c3e79baa9328f33080f0
Live session user <claucea97@gmail.com>
2017-09-21 23:06:58 +0000

    odio los lunes

--- p1/practica1.c
+++ p1/practica1.c
@@ -1,11 +1,11 @@
 /***************************************************************************
- EjemploPcapNext.c
- Muestra el tiempo de llegada de los primeros 500 paquetes a la interface eth0
-y los vuelca a traza (¿correctamente?) nueva con tiempo actual
+practica1.c
+Programa que dependiendo del numero de argumentos de entrada o muestra los 
+paquetes capturados con fecha modificada, o muestra los paquetes con una determinada
+traza. En ambos casos, el programa muestra el argumento N.
 
- Compila: gcc -Wall -o EjemploPcapNextEx EjemploPcapNextEx.c -lpcap
- Autor: Jose Luis Garcia Dorado
- 2017 EPS-UAM
+Compila: gcc -Wall -o EjemploPcapNextEx EjemploPcapNextEx.c -lpcap
+Autor: Claudia Cea, Juan Riera
 ***************************************************************************/
 
 #ifdef HAVE_CONFIG_H
@@ -21,11 +21,15 @@ y los vuelca a traza (¿correctamente?) nueva con tiempo actual
 #include <linux/tcp.h>
 #include <signal.h>
 #include <time.h>
+
 #define OK 0
 #define ERROR 1
 
 #define ETH_FRAME_MAX 1514	// Tamanio maximo trama ethernet
-#define MAXBUF 512		// Tamanio maximo de N (primer argumento)
+#define MAXBUF 512		// Tamanio maximo de primer argumento
+
+
+/* Variables globales */
 
 pcap_t *descr=NULL,*descr2=NULL;
 pcap_dumper_t *pdumper=NULL;
@@ -46,7 +50,7 @@ int imprimir_paquete(int paquete, int N){
 		for(i=0;i*2<strlen(buffer);i++){
 			printf("%c", buffer[2*i]);
 			if(i*2+1<strlen(buffer)){
-				printf("%c ", buffer[2*i+1];
+				printf("%c ", buffer[2*i+1]);
 			}
 		}
 	} else {
@@ -70,25 +74,26 @@ void handle(int nsignal){
 	exit(OK);
  }
 
-int main(int argc, char **argv)
-{
 
-	int retorno=0, N;
+int main(int argc, char **argv){
+
+	int retorno=0;
+	int N;
 	char errbuf[PCAP_ERRBUF_SIZE];
 	uint8_t *paquete=NULL;
 	struct pcap_pkthdr *cabecera=NULL;
 	char file_name[256];
 	struct timeval time;
 
-    if(argc<2){
-        printf("\n\n\tError al introducir comandos.\n"
+	/*Caso 1*/
+    	if(argc<2){
+       		printf("\n\n\tError al introducir comandos.\n"
 		"Instrucciones: \n--Para inciar una captura introducir:\n\n\t"
 		"./practia1 N\n\nDonde N será el número de bytes a mostrar\n"
 		"de cada paquete.\n\n--Para leer una traza introducir:\n\n\t"
 		"./practica1 N name\n\nDonde N será el número de bytes a mostrar\n"
 		"de cada paquete y name el nombre de la traza.\n\n");
-        exit(EXIT_SUCCESS);
-
+        	exit(EXIT_SUCCESS);
         }
 	
 	if(signal(SIGINT,handle)==SIG_ERR){
@@ -98,19 +103,22 @@ int main(int argc, char **argv)
 
 	N=atoi(argv[1]);
 
-    if(argc==2){
+	/*Caso 2*/
+    	if(argc==2){
 		//Apertura de interface
-   	if ((descr = pcap_open_live("eth0",9,0,100, errbuf)) == NULL){
+   		if ((descr = pcap_open_live("eth0",9,0,100, errbuf)) == NULL){
 		printf("Error: pcap_open_live(): %s, %s %d.\n",errbuf,__FILE__,__LINE__);
 		exit(ERROR);
 	}
-		//Volcado de traza
+
+	//Volcado de traza
 	descr2=pcap_open_dead(DLT_EN10MB,ETH_FRAME_MAX);
 	if (!descr2){
 		printf("Error al abrir el dump.\n");
 		pcap_close(descr);
 		exit(ERROR);
 	}
+
 	gettimeofday(&time,NULL);
 	sprintf(file_name,"eth0.%lld.pcap",(long long)time.tv_sec);
 	pdumper=pcap_dump_open(descr2,file_name);
@@ -119,7 +127,6 @@ int main(int argc, char **argv)
 		pcap_close(descr);
 		pcap_close(descr2);
 	}
-
 
 	while (1){
 		retorno = pcap_next_ex(descr,&cabecera,(const u_char **)&paquete);
@@ -136,7 +143,7 @@ int main(int argc, char **argv)
 		else if(retorno==-2){
 			break;
 		}
-			//En otro caso
+		//En otro caso
 		contador++;
 		printf("Nuevo paquete capturado a las %s\n",ctime((const time_t*)&(cabecera->ts.tv_sec)));
 		cabecera->ts.tv_sec+=172800;
@@ -147,8 +154,7 @@ int main(int argc, char **argv)
 			break;
 		}
 		
-        if(pdumper){
-
+        	if(pdumper){
 			pcap_dump((uint8_t *)pdumper,cabecera,paquete);
 		}
 	}
