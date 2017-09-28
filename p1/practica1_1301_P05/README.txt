3b992cfcd3eaa332bef917f849a67684ddd1496c
juan260 <superjuan260@hotmail.com>
2017-09-28 17:46:13 +0200

    PRACTICA 1 ENTRGADAAAA

---
+++ p1/practica1_1301_P05/README.txt
@@ -0,0 +1,5 @@
+Autores: Juan Riera Gomez y Claudia Cea Tassel
+Pareja 5, grupo 1301
+Para compilar el ejercicio 1, ejecutar make sobre la carpeta raiz.
+En cuanto al ejercicio 2, hemos respondido en el documento "Ejercicios de captura de trafico.pdf"
+El resto de archivos son adjuntos de capturas de los distintos ejercicios de captura.
---
+++ p1/practica1_1301_P05/practica1.c
@@ -0,0 +1,193 @@
+/***************************************************************************
+practica1.c
+Programa que dependiendo del numero de argumentos de entrada o muestra los 
+paquetes capturados con fecha modificada, o muestra los paquetes con una determinada
+traza. En ambos casos, el programa muestra el argumento N.
+
+Compila: gcc -Wall -o EjemploPcapNextEx EjemploPcapNextEx.c -lpcap
+Autor: Claudia Cea, Juan Riera
+***************************************************************************/
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <pcap.h>
+#include <string.h>
+#include <netinet/in.h>
+#include <linux/udp.h>
+#include <linux/tcp.h>
+#include <signal.h>
+#include <time.h>
+
+#define OK 0
+#define ERROR 1
+
+#define ETH_FRAME_MAX 1514	// Tamanio maximo trama ethernet
+#define MAXBUF 1024		// Tamanio maximo de primer argumento
+
+
+/* Variables globales */
+
+pcap_t *descr=NULL,*descr2=NULL;
+pcap_dumper_t *pdumper=NULL;
+
+int contador =0;
+
+/**
+*	Funcion para imprimir el paquete que devuelve 1 en caso de error
+**/
+
+void imprimir_paquete(uint8_t *paquete, int N, int len){
+    int i;
+    if(N>len){
+        printf("Paquete demasiado corto, imprimiento paquete entero:\n");
+        for(i=0;i<len;i++){
+            printf("%02x ", paquete[i]);
+        }
+    } else {
+        for(i=0;i<N;i++){
+            printf("%02x ", paquete[i]);
+        }
+    }
+    printf("\n");
+}
+
+void handle(int nsignal){
+	printf("\nControl C pulsado\n");
+	printf("Paquetes capturados: %d\n", contador);
+	if(descr)
+		pcap_close(descr);
+	if(descr2)
+		pcap_close(descr2);
+	if(pdumper)
+		pcap_dump_close(pdumper);
+	exit(OK);
+ }
+
+
+int main(int argc, char **argv){
+
+	int retorno=0;
+	int N;
+	char errbuf[PCAP_ERRBUF_SIZE];
+	uint8_t *paquete=NULL;
+	struct pcap_pkthdr *cabecera=NULL;
+	char file_name[256];
+	struct timeval time;
+
+
+    	if(argc<2||argc>3){
+       		printf("\n\n\tError al introducir comandos.\n"
+		"Instrucciones: \n--Para inciar una captura introducir:\n\n\t"
+		"./practia1 N\n\nDonde N será el número de bytes a mostrar\n"
+		"de cada paquete.\n\n--Para leer una traza introducir:\n\n\t"
+		"./practica1 N name\n\nDonde N será el número de bytes a mostrar\n"
+		"de cada paquete y name el nombre de la traza.\n\n");
+        	exit(EXIT_SUCCESS);
+        }
+	
+	if(signal(SIGINT,handle)==SIG_ERR){
+		printf("Error: Fallo al capturar la senal SIGINT.\n");
+		exit(ERROR);
+	}	
+
+	N=atoi(argv[1]);
+
+
+    if(argc==2){
+		    //Apertura de interface
+       	if ((descr = pcap_open_live("wlp4s0",MAXBUF,0,100, errbuf)) == NULL){
+		    printf("Error: pcap_open_live(): %s, %s %d.\n",errbuf,__FILE__,__LINE__);
+		    exit(ERROR);
+	    }
+		    //Volcado de traza
+	    descr2=pcap_open_dead(DLT_EN10MB,ETH_FRAME_MAX);
+	    if (!descr2){
+		    printf("Error al abrir el dump.\n");
+		    pcap_close(descr);
+		    exit(ERROR);
+	    }
+	    gettimeofday(&time,NULL);
+	    sprintf(file_name,"eth0.%lld.pcap",(long long)time.tv_sec);
+	    pdumper=pcap_dump_open(descr2,file_name);
+	    if(!pdumper){
+		    printf("Error al abrir el dumper: %s, %s %d.\n",pcap_geterr(descr2),__FILE__,__LINE__);
+		    pcap_close(descr);
+		    pcap_close(descr2);
+	    }
+
+
+	    while (1){
+		    retorno = pcap_next_ex(descr,&cabecera,(const u_char **)&paquete);
+		    if(retorno == -1){ 		//En caso de error
+			    printf("Error al capturar un paquete %s, %s %d.\n",pcap_geterr(descr),__FILE__,__LINE__);
+			    pcap_close(descr);
+			    pcap_close(descr2);
+			    pcap_dump_close(pdumper);
+			    exit(ERROR);
+		    }
+		    else if(retorno == 0){
+			    continue;
+		    }
+		    else if(retorno==-2){
+			    break;
+		    }
+			    //En otro caso
+		    contador++;
+		    printf("Nuevo paquete capturado a las %s\nContenido del paquete: %u\n\n",
+		        ctime((const time_t*)&(cabecera->ts.tv_sec)), *paquete);
+		        
+		    cabecera->ts.tv_sec+=172800;
+            	printf("Paquete capturado con fecha editada %s\n",ctime((const time_t*)&(cabecera->ts.tv_sec)));
+
+		
+		    imprimir_paquete(paquete, N, cabecera->len);
+		
+            if(pdumper){
+
+			    pcap_dump((uint8_t *)pdumper,cabecera,paquete);
+		    }
+	    }
+	    pcap_close(descr2);
+	    pcap_dump_close(pdumper);
+	}   else    {
+        	// En caso de que haya dos argumentos
+        	descr = pcap_open_offline(argv[2], errbuf);
+        	if(descr==NULL){
+              		printf("Error: pcap_open_offline(): %s, %s %d.\n",errbuf,__FILE__,__LINE__);
+              		exit(ERROR);
+        	}
+        	
+        	while (1){
+		        retorno = pcap_next_ex(descr,&cabecera,(const u_char **)&paquete);
+		        if(retorno == -1){ 		//En caso de error
+			        printf("Error al capturar un paquete %s, %s %d.\n",pcap_geterr(descr),__FILE__,__LINE__);
+			        pcap_close(descr);
+			        pcap_close(descr2);
+			        pcap_dump_close(pdumper);
+			        exit(ERROR);
+		        }
+		        else if(retorno == 0){
+			        continue;
+		        }
+		        else if(retorno==-2){
+			        break;
+		        }
+			        //En otro caso
+		        contador++;
+		        printf("Nuevo paquete capturado a las %s\nContenido del paquete: %u\n\n",
+		            ctime((const time_t*)&(cabecera->ts.tv_sec)), *paquete);
+		
+		        imprimir_paquete(paquete, N, cabecera->len);
+	        }
+    	}
+	pcap_close(descr);
+	
+
+
+	return OK;
+}
+
