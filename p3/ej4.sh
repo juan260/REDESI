472d17edae12458c6b76fb030f235a369566d46e
Juan Riera Gomez <superjuan260@hotmail.com>
2017-11-14 12:59:15 +0100

    asdf

--- p3/ej4.sh
+++ p3/ej4.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 # Recibe como argumento el nombre de una traza y devuelve los anchos de banda en un archivo 
-# que recibe como segundo argumento
-echo
-echo "Calculo de los anchos de banda"
-
-
+# que recibe como segundo argumento. Los resultados los exporta en dos graficas con nombres
+# "graficaTasaEntrada.jpeg" y graficaTasDeSalida.jpeg"
+#echo
+#echo "Calculo de los anchos de banda"
 
 #Analiza la tasa o ancho de banda de la traza que recibe como argumento en bits
 
@@ -18,12 +17,13 @@ then
 tshark -r $1 -qz io,stat,1,"SUM(frame.len)frame.len&&eth.src==00:11:88:CC:33:21" > tasasout.tmp
 fi
 
-sed "1,12d" tasasin.tmp |awk '{print $6*8}'| awk '{printf("%d\t\t%d\n", NR, $1);}' > datostasain.tmp
+sed "1,12d" tasasin.tmp |awk '{print $6*8}'| awk '{printf("%d\t\t%d\n", NR, $1);}' > datostasa.tmp
+./grafica.sh "Ancho de banda de salida en cada segundo" "Ancho (bits/seg)" "Segundo" "datostasa.tmp" "graficaTasaDeEntrada.jpeg"
 
+sed "1,12d" tasasout.tmp |awk '{print $6*8}'| awk '{printf("%d\t\t%d\n", NR, $1);}' > datostasa.tmp
+./grafica.sh "Ancho de banda de entrada en cada segundo" "Ancho (bits/seg)" "Segundo" "datostasa.tmp" "graficaTasaDeSalida.jpeg"
 
-sed "1,12d" tasasout.tmp |awk '{print $6*8}'| awk '{printf("%d\t\t%d\n", NR, $1);}' > datostasaout.tmp
-
-rm -f datostasaout.tmp datostasain.tmp
+rm -f datostasa.tmp
 #./tasa.sh traza.pcap | awk 'BEGIN{printf("Intervalo\tAncho\n");} {printf("%d\t\t%d\n", NR, $1);}'
 
 
--- p3/grafica.sh
+++ p3/grafica.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
+#Generador de graficas que recibe como argumentos:
+#	-Titulo de la grafica
+#	-Nombre del eje X
+#	-Nombre del eje Y
+#	-Nombre del archivo de entrada
+#	-Nombre del archivo de salida
 gnuplot -persist -e "set title \"$1\"; set xlabel \"$2\"; set ylabel \"$3\";" -e "set term jpeg; set output \"$5\"; plot \"$4\" using 1:2 with lines;"
 
